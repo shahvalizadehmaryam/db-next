010525429a5b1f06ba9385bd088d2fd24043265b
Maryam Shahvalizadeh <Shahvalizadehmaryam@gmail.com.>
2025-01-04 11:12:28 +0330

    method post for sending name to db developp

---
+++ pages/api/data/index.js
@@ -0,0 +1,12 @@
+export default function handler(req, res) {
+  if (req.method === "POST") {
+    const { name } = req.body;
+    if (!name || name.length <= 3) {
+      res.status(422).json({ status: "failed", message: "Invalid data" });
+      return;
+    }
+    res
+      .status(201)
+      .json({ status: "success", message: "posted successfully", data: name });
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
+import { useState } from "react";
+
 export default function Home() {
+  const [name, setName] = useState("");
+  const postHandler = async () => {
+    const res = await fetch("/api/data", {
+      method: "POST",
+      body: JSON.stringify({ name }),
+      headers: { "Content-Type": "application/json" },
+    });
+    const data = await res.json();
+    console.log(data);
+  };
   return (
     <div>
       <h3>Connecting database to Next</h3>
+      <div>
+        <input value={name} onChange={(e) => setName(e.target.value)} />
+        <button onClick={postHandler}>Post</button>
+      </div>
     </div>
   );
 }
