2a11cad48ea647ded41d9bec4f4fec0598ca2825
Maryam Shahvalizadeh <Shahvalizadehmaryam@gmail.com.>
2025-01-06 09:46:46 +0330

    connected to db via mongoose.

--- pages/api/data/index.js
+++ pages/api/data/index.js
@@ -1,12 +1,18 @@
-export default function handler(req, res) {
+import mongoose from "mongoose";
+import connectDb from "../../../utils/ConnectDb";
+
+export default async function handler(req, res) {
+  await connectDb();
   if (req.method === "POST") {
     const { name } = req.body;
     if (!name || name.length <= 3) {
       res.status(422).json({ status: "failed", message: "Invalid data" });
       return;
     }
-    res
-      .status(201)
-      .json({ status: "success", message: "posted successfully", data: name });
+    res.status(201).json({
+      status: "success",
+      message: "posted successfully",
+      data: { name },
+    });
   }
 }
---
+++ utils/connectDb.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+async function connectDb() {
+  try {
+    // if it is connected so don't do anything
+    if (mongoose.connections[0].readyState) return;
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("connected to db");
+  } catch (error) {
+    console.log("Failed to connect db");
+    console.log(error);
+  }
+}
+export default connectDb;
