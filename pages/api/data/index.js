0864e624ecc2b24c49dcb812ed24607bc367e64b
Maryam Shahvalizadeh <Shahvalizadehmaryam@gmail.com.>
2025-01-06 10:14:11 +0330

    stored to db successfully.

--- pages/api/data/index.js
+++ pages/api/data/index.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import connectDb from "../../../utils/ConnectDb";
+import User from "../../../models/User";
 
 export default async function handler(req, res) {
   await connectDb();
@@ -9,10 +10,12 @@ export default async function handler(req, res) {
       res.status(422).json({ status: "failed", message: "Invalid data" });
       return;
     }
+    const user = await User.create({ name });
+    console.log(user);
     res.status(201).json({
       status: "success",
       message: "posted successfully",
-      data: { name },
+      data: user,
     });
   }
 }
